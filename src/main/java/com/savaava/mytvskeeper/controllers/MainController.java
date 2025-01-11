c9a27fffa214fafc4c9d0fca12f8e8a5e70e8486
savaava <andreasavastano4388@gmail.com>
2025-01-11 18:23:04 +0100

    Refactor MainController to use LocalDateTime, add Label to FXML, and update .gitignore

--- src/main/java/com/savaava/mytvskeeper/controllers/MainController.java
+++ src/main/java/com/savaava/mytvskeeper/controllers/MainController.java
@@ -5,10 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.io.*;
 import java.net.URL;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
@@ -16,6 +17,8 @@ public class MainController implements Initializable {
     public Button b;
     @FXML
     public Button btnProva;
+    @FXML
+    public Label lbl;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -24,19 +27,19 @@ public class MainController implements Initializable {
 
     @FXML
     public void testIOExceptionMethod() {
-        LocalDate date = null;
-        try(ObjectInputStream oos = new ObjectInputStream(new BufferedInputStream(new FileInputStream("./executable/bin/data.bin")))){
-            date = (LocalDate)oos.readObject();
+        LocalDateTime date = null;
+        try(ObjectInputStream oos = new ObjectInputStream(new BufferedInputStream(new FileInputStream("./bin/data.bin")))){
+            date = (LocalDateTime) oos.readObject();
         } catch (IOException | ClassNotFoundException ex) {
             new AlertError("Error in testIOExceptionMethod", "Error's details: "+ex.getMessage());
         }
-        System.out.println(date);
+        lbl.setText(date!=null?date.toString():"no informations");
     }
 
     @FXML
-    public void onSaveBin(ActionEvent actionEvent) {
-        try(ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("./executable/bin/data.bin")))){
-            oos.writeObject(LocalDate.of(2000,11,15));
+    public void onSaveBin() {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("./bin/data.bin")))){
+            oos.writeObject(LocalDateTime.now());
         }catch (IOException ex) {
             new AlertError("Error in onSaveBin", "Error's details: "+ex.getMessage());
         }
